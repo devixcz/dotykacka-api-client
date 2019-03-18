04ef9ff26c9dbc35dc79a8fc479db2af550f9651
Jan Cinert <jan.cinert@gmail.com>
2019-03-18 19:55:13 +0100

    Fixed multipart/form-data

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -46,8 +46,8 @@ class ApiClient
         );
 
         $uri = new Uri($this->apiBaseUrl.'/'.$path);
+        $contentType = null;
         if ($files) {
-            $contentType = 'multipart/form-data';
             $options = array();
             foreach ($files as $file) {
                 $options[] = array(
@@ -84,9 +84,9 @@ class ApiClient
         $request = $factory->createRequest(
                 $method,
                 $uri,
-                [
+                $contentType ? [
                         'Content-Type' => array($contentType),
-                ],
+                ] : [],
                 $body
         );
 
